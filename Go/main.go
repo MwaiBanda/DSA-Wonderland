89271574326c29d031e41cb5cbfa1b1e2d4d00c2
Mwai Banda <mwai.banda@cbn.org>
2023-10-17 23:58:07 -0500

    Go: Added binary search

--- Go/main.go
+++ Go/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+//	"strconv"
+)
 
 func main() {
-	fmt.Println("Hello from Go!")
+	needToSort := []int{10, 2, 3, 5, 7, 1}
+	fmt.Println("Before Binary Search:", needToSort)
+	fmt.Println("After Binary Search:", binarySearch(needToSort...))
+}
+
+func binarySearch(arr ...int) []int {
+	for i,_ := range arr {
+		for j := 0; j < (len(arr) - 1) - i; j++ {
+			if arr[j] > arr[j + 1] {
+				temp := arr[j + 1]
+				arr[j + 1] = arr[j]
+				arr[j] = temp
+			}
+		}
+//		fmt.Println("Pass No" + strconv.Itoa(i+ 1), arr)
+	}
+	return arr
 }
